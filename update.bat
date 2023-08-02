ac5dd722b942635bf0540735918b97e0bdbbc7e6
Cold Hands <cold@coldhands.xyz>
2023-08-01 23:57:22 -0400

    updates

--- update.bat
+++ update.bat
@@ -1,12 +1,15 @@
 @echo off
-busybox mv -f ffmpeg.exe ffmpeg-yt.exe & ^
-busybox mv -f ffmpeg-git.exe ffmpeg.exe & ^
-powershell -nol -nop ./updater.ps1 & ^
+echo Searching for updates . . .
+busybox rm ffmpeg* & ^
+powershell -nol -nop updater.ps1 & ^
 busybox mv -f ffmpeg.exe ffmpeg-git.exe & ^
-busybox mv -f ffmpeg-yt.exe ffmpeg.exe & ^
+busybox mv -f installer/updater.ps1 . & ^
+busybox rm -rf doc mpv installer d3dcompiler_*.dll updater.bat & ^
 busybox wget "https://github.com/yt-dlp/ffmpeg-builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip" & ^
 busybox unzip -oq ffmpeg-master-latest-win64-gpl.zip & ^
 busybox mv -f ffmpeg-master-latest-win64-gpl/bin/* . & ^
 busybox rm -rf ffmpeg-master-latest-win64-gpl* & ^
 yt-dlp --update-to nightly & ^
-echo ok
+echo --------------------------------
+echo Update complete!
+pause
